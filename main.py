5f486e44c821ecd0634378b46a5f03b103cc2025
jonasliendl <57270170+jonasliendl@users.noreply.github.com>
2024-08-04 13:52:11 +0200

    feat: pearson distance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from utils.k_nearest_neighbour import get_k_nearest_neighbours
-from algorithms.cosine_distance import calculate_distance
+from algorithms.pearson_distance import calculate_distance
 
 data = [[1, 2, 3], [10, 40, 90], [2, 6, 10], [12.4, 23.4, 1.3], [0.5, 1.2, 3.4]]
 
