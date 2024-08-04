e4195dc8d4eb4581361f934f21c73704abdade02
jonasliendl <57270170+jonasliendl@users.noreply.github.com>
2024-08-04 13:28:38 +0200

    feat: cosine distance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from utils.k_nearest_neighbour import get_k_nearest_neighbours
-from algorithms.squared_euclidian_distance import calculate_distance
+from algorithms.cosine_distance import calculate_distance
 
-data = [[1, 2, 4], [4, 9, 1], [12.4, 99.6, 19.2], [0.45, 12.2, 45.5], [1, 4.5, 2.3]]
+data = [[1, 2, 3], [10, 40, 90], [2, 6, 10], [12.4, 23.4, 1.3], [0.5, 1.2, 3.4]]
 
-print(get_k_nearest_neighbours([1, 2, 3], data, calculate_distance))
+print(get_k_nearest_neighbours([1, 4, 9], data, calculate_distance, 5))
